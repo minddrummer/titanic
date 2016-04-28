2a659ce8f277103ad39c99a2472f3666d21adbc9
Ke <kesang0156357@gmail.com>
2016-04-27 23:36:33 -0400

    add function to create Dummy in pd Dataframe with train/test frameowrk

--- ensemble.py
+++ ensemble.py
@@ -37,10 +37,71 @@ data = pd.read_csv('train.csv')
 
 ######feature engineering
 ######SibSp: as int64, for all algorithm
-add binary variable based on SibSp?
+# add binary variable based on SibSp 
+#binary data no need for dummy variables
+class NominalSibSp(BaseEstimator, TransformerMixin):
+	'''this class divided the SibSp into different class by the mean'''
+	def __init__(self):
+		self.mean = np.NaN
+	def fit(self, X, y=None):
+		self.mean = X.loc[:,'SibSp'].mean()
+		return self
+	def transform(self, X, y=None):
+		X.loc[:, 'SibSp_binary'] = (X.loc[:,'SibSp']>=self.mean).astype(int)
+		return X
+
 ######Parch: as int64, for all algorithm
+# add binary variable based on Parch  
+#binary data no need for dummy variables
+class NominalParch(BaseEstimator, TransformerMixin):
+	'''this class divided the SibSp into different class by the mean'''
+	def __init__(self):
+		self.mean = np.NaN
+	def fit(self, X, y=None):
+		self.mean = X.loc[:,'Parch'].mean()
+		return self
+	def transform(self, X, y=None):
+		X.loc[:, 'Parch_binary'] = (X.loc[:,'Parch']>=self.mean).astype(int)
+		return X
+
 ######Pclass: nominal varibale, should transform to dummy variable if needed for all algorithm
-should we transform to dummy V even it is for RF?
+# should we transform to dummy V even it is for RandomForestModel? Yes, for convenience
+# and more importantly, should based on training, not testing: how to achieve it?
+class CreateDummy(BaseEstimator, TransformerMixin):
+	'''
+	this class create dummy variables for any nominal variable in pandas Dataframe,
+	and when transform the new data, if any label is not known, it will be classified as all Os
+	it will create all variables corresponding to all levels in pd.DataFrame framework;
+	this class doesnot handle missing value: the best is to fill in before use this class; if not,
+	it will create NaN binary variable, but the binary name is not proper
+	'''
+	def __init__(self):
+		self.binary_labels = None
+		self.root_name = None
+
+	def fit(self, X, y = None):
+		self.binary_labels = list(X.unique())
+		self.root_name = X.name
+		self.var_names = [self.root_name+'_'+ str(binary_label) for binary_label in self.binary_labels]
+
+	def transform(self, X, y = None):
+		# print self.var_names
+		res = pd.DataFrame(columns = self.var_names, index = X.index)
+		res.loc[:,:] = 0
+		# print res
+		res.loc[:, 'VarName'] = X.apply(lambda x: self.root_name+'_'+str(x))
+		# print res	
+		def match_binary_var_be_0(row):
+			if row.VarName in row.index:
+				row.loc[row.VarName] = 1
+			return row
+		res = res.apply(match_binary_var_be_0, axis=1)
+		res.drop('VarName', axis=1, inplace=True)
+		return res
+
+create_dummy = CreateDummy()
+create_dummy.fit(data.Pclass)
+create_dummy.transform(data.Pclass)
 ######Sex
 
 ######Age
@@ -62,4 +123,8 @@ should we transform to dummy V even it is for RF?
 
 
 #####for some ML, we need to transfer to dummy variables
+
+
+
+
 
