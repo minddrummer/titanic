2dad359003cf154dce19412999d5ee69b8d4b9b3
Ke <kesang0156357@gmail.com>
2016-06-02 18:02:41 -0400

    svm model cv

--- ensemble.py
+++ ensemble.py
@@ -19,6 +19,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import Ridge, Lasso, ElasticNet
 import statsmodels.formula.api as smf
+from sklearn.preprocessing import StandardScaler as Scaler
+from sklearn.svm import SVC
 pylab.ion()
 pd.set_option('display.max_rows', 2000)
 pd.set_option('display.max_columns', 200)
@@ -176,16 +178,16 @@ class ProcessTicket(BaseEstimator, TransformerMixin):
 	'''
 	this function transfer the ticket number to length(need further categorize to dummy V)
 	'''
-	def __init__(self):
-		pass
+	def __init__(self, ticket_start = False):
+		self.ticket_start = ticket_start
 	def fit(self, X, y=None, **fit_paras):
 		return self
 	def transform(self, X0, y=None, **transform_paras):
 		X = X0.copy()
 		tickets = X.Ticket.apply(lambda row: self.extract_letter(row))
 		X.loc[:,'Ticket_len'] = tickets.apply(lambda x: len(x))
-		# X.loc[:,'Ticket_start'] = tickets.apply(lambda x: x[0] if len(x)>0 else '0')
-		# X.drop('Ticket', axis=1, inplace=True)
+		if self.ticket_start:
+			X.loc[:,'Ticket_start'] = tickets.apply(lambda x: x[0] if len(x)>0 else '0')
 		return X
 	def extract_letter(self, row):
 		# m = re.search('^[A-Za-z]+.* ', row)
@@ -253,10 +255,13 @@ class ProcessCabin(BaseEstimator, TransformerMixin):
 		X = X0.copy()
 		cabins = X.Cabin.apply(lambda row: row if type(row) is str else '')
 		X.loc[:, 'Cabin'] = cabins.apply(lambda row: self.parse_cabin(row))
+		# print X.Cabin
 		return X
 	def parse_cabin(self, row):
 		m = re.search('^[A-Za-z]+?', row)
-		if m: return m.group(0)
+		if m: 
+			if m.group(0) != 'T': return m.group(0)
+			else: return 'unknown'
 		else: return 'unknown'
 
 class ProcessCabin2(BaseEstimator, TransformerMixin):
@@ -371,96 +376,209 @@ train = data[select].copy()
 valid = data[select.apply(lambda x: not x)].copy()
 
 ###miscellaneous
+train.set_index(['PassengerId'], inplace=True)
+valid.set_index(['PassengerId'], inplace=True)
+test.set_index(['PassengerId'], inplace=True)
 y_train = train.Survived
 y_valid = valid.Survived
 train.drop('Survived', inplace = True, axis=1)
 valid.drop('Survived', inplace = True, axis=1)
 test.loc[:,'Fare'].fillna(value = train.loc[:,'Fare'].mean(), inplace=True)
-test_passengerId = test.PassengerId
-train.drop(['PassengerId'], axis=1, inplace=True)
-valid.drop(['PassengerId'], axis=1, inplace=True)
-test.drop(['PassengerId'], axis=1, inplace=True)
+# test_passengerId = test.PassengerId
 ##combine train and valid for combined fitting after gridsearchCV
 df = pd.concat([train, valid], axis=0).copy()
 
-# ###pipeline transformation for random forest
+
+######################################random forest######################################
+cross_validation_rf = False
+# cross_validation_rf = True
+
 # categorical_vars = ['Pclass','Name','Cabin','Embarked','Ticket_start', 'Ticket']
 categorical_vars = ['Pclass','Name','Cabin','Embarked', 'Ticket']
 
-####
-# pip_rf=make_pipeline(ProcessFare(), NominalSibSp(), NominalParch(), TransformSex(), ExtractName(),ProcessTicket(), ProcessTicket2(),\
+#####pipeline transformation for random forest
+# pip_rf=make_pipeline(ProcessFare(), NominalSibSp(), NominalParch(), TransformSex(), ExtractName(), ProcessTicket(), ProcessTicket2(False),\
 # 	ProcessEmbarked(), ProcessCabin(), CombineDummyVars(categorical_vars), LinearRegForAge())
 
-pip_rf=make_pipeline(ProcessFare(), NominalSibSp(), NominalParch(), TransformSex(), ExtractName(), ProcessTicket(), ProcessTicket2(False),\
+pip_rf=make_pipeline(ProcessFare(), NominalSibSp(), NominalParch(), TransformSex(), \
+	ExtractName(), ProcessTicket(ticket_start=False), ProcessTicket2(drop=False),\
 	ProcessEmbarked(), ProcessCabin(), CombineDummyVars(categorical_vars), LinearRegForAge())
 
-####pipline test
 
 
-print 'first print, data df shape is (%d, %d)' %(train.shape[0], train.shape[1])
-train = pip_rf.fit_transform(train)
-print 'second print, data df shape is (%d, %d)' %(train.shape[0], train.shape[1])
-valid = pip_rf.transform(valid)
+if cross_validation_rf:
+	print 'first print, data df shape is (%d, %d)' %(train.shape[0], train.shape[1])
+	train = pip_rf.fit_transform(train)
+	print 'second print, data df shape is (%d, %d)' %(train.shape[0], train.shape[1])
+	valid = pip_rf.transform(valid)
+
+	######best parameter setting so far
+	# pip_rf=make_pipeline(ProcessFare(), NominalSibSp(), NominalParch(), TransformSex(), ExtractName(), ProcessTicket(), ProcessTicket2(False),\
+	# ProcessEmbarked(), ProcessCabin(), CombineDummyVars(categorical_vars), LinearRegForAge())
+
+# the best parameter setting is: RandomForestClassifier(bootstrap=True, compute_importances=None,
+#             criterion='gini', max_depth=None, max_features=9,
+#             max_leaf_nodes=None, min_density=None, min_samples_leaf=1,
+#             min_samples_split=11, n_estimators=100, n_jobs=1,
+#             oob_score=True, random_state=None, verbose=0)
+# the best CV score of the GridSearchCV is: 0.844537815126
+# the best oob score of the best estimator of grid search results are: 0.830532212885
+# the in-bag prediction accuracy rate is: 0.903361344538
+# the validation prediction accuracy rate is: 0.819209039548
+	######best parameter setting so far
+
+	# # ###random forest classifier
+	rfc = RandomForestClassifier(oob_score=True, max_depth=None)
+	# #parameter grids: 
+	n_estimators = [50, 100, 150, 200]
+	max_features = [3, 6, 9, 15,20, 25]
+	# max_features = [20,25,30,40]
+	min_samples_split = [2, 3, 6, 9, 11, 15]
 
 
-
-# # # ###random forest classifier
-# rfc = RandomForestClassifier(oob_score=True, max_depth=None)
-# # #parameter grids: 
-# n_estimators = [50, 100, 160, 200]
-# max_features = [3, 6, 9, 15]
-# # max_features = [20,25,30,40]
-# min_samples_split = [2, 8, 9, 11]
+	#parameter grids: 
+	# n_estimators = [125]
+	# max_features = [20,25,30]
+	# # max_features = [10,20,25,40,60]
+	# min_samples_split = [9]
 
 
-# #parameter grids: 
-# # n_estimators = [125]
-# # max_features = [20,25,30]
-# # # max_features = [10,20,25,40,60]
-# # min_samples_split = [9]
+	rf_paras = {'n_estimators': n_estimators, 'max_features':max_features,'min_samples_split':min_samples_split}
+	rfc_grid_search = GridSearchCV(rfc, param_grid = rf_paras, cv = 7, refit=True)
+	rfc_grid_search.fit(train, y_train)
+
+	y_predict = rfc_grid_search.predict(train)
+	y_valid_predict = rfc_grid_search.predict(valid)
+	# y_test = rfc_grid_search.predict(test)
+
+	# ###checking results
+	print 'the combinations scores of each parameter setting:', sorted(rfc_grid_search.grid_scores_, key=lambda x: x[1])
+	print 'the best parameter setting is:', rfc_grid_search.best_estimator_
+	print 'the best CV score of the GridSearchCV is:', rfc_grid_search.best_score_
+	#best CV score is:
+	print 'the best oob score of the best estimator of grid search results are:', rfc_grid_search.best_estimator_.oob_score_
+	print 'the in-bag prediction accuracy rate is:', (y_train == y_predict).sum()/float(y_train.shape[0])
+	print 'the validation prediction accuracy rate is:', (y_valid == y_valid_predict).sum()/float(y_valid.shape[0])
+	# print 'the feature importances are:', sorted(list(zip(train.columns, rfc_grid_search.best_estimator_.feature_importances_)), key = lambda x: x[1], reverse=True)
 
 
-# rf_paras = {'n_estimators': n_estimators, 'max_features':max_features,'min_samples_split':min_samples_split}
-
-# rfc_grid_search = GridSearchCV(rfc, param_grid = rf_paras, cv = 7, refit=True)
-# rfc_grid_search.fit(train, y_train)
-
-# y_predict = rfc_grid_search.predict(train)
-# y_valid_predict = rfc_grid_search.predict(valid)
-# # y_test = rfc_grid_search.predict(test)
-
-# # ###checking results
-# print 'the combinations scores of each parameter setting:', sorted(rfc_grid_search.grid_scores_, key=lambda x: x[1])
-# print 'the best parameter setting is:', rfc_grid_search.best_estimator_
-# print 'the best CV score of the GridSearchCV is:', rfc_grid_search.best_score_
-# #best CV score is:
-# print 'the best oob score of the best estimator of grid search results are:', rfc_grid_search.best_estimator_.oob_score_
-# print 'the in-bag prediction accuracy rate is:', (y_train == y_predict).sum()/float(y_train.shape[0])
-# print 'the validation prediction accuracy rate is:', (y_valid == y_valid_predict).sum()/float(y_valid.shape[0])
-# # print 'the feature importances are:', sorted(list(zip(train.columns, rfc_grid_search.best_estimator_.feature_importances_)), key = lambda x: x[1], reverse=True)
+else:
+	# #####combine train and valid to get the final model and predict again
+	rfc_self = RandomForestClassifier(oob_score=True, max_depth=None,max_features=9, min_samples_split=11, n_estimators=100)
+	# final_df = pd.concat([train, valid], axis = 0)
+	final_df = pip_rf.fit_transform(df)
+	final_y = pd.concat([y_train, y_valid], axis=0).reset_index().set_index('PassengerId').sort().loc[:,'Survived']
+	rfc_self.fit(final_df,final_y)
+	print rfc_self.oob_score_
+	y_predict = rfc_self.predict(final_df)
+	print 'the in-bag prediction accuracy rate of random forest is:', (final_y == y_predict).sum()/float(final_y.shape[0])
+	test_rf = pip_rf.transform(test)
+	y_test = rfc_self.predict(test_rf)
 
 
-# #####combine train and valid to get the final model and predict again
-rfc_self = RandomForestClassifier(oob_score=True, max_depth=None,max_features=9, min_samples_split=11, n_estimators=160)
-# final_df = pd.concat([train, valid], axis = 0)
-final_df = pip_rf.fit_transform(df)
-final_y = pd.concat([y_train, y_valid], axis=0).reset_index().set_index('index').sort().loc[:,'Survived']
-rfc_self.fit(final_df,final_y)
-print rfc_self.oob_score_
-y_predict = rfc_self.predict(final_df)
-print 'the in-bag prediction accuracy rate is:', (final_y == y_predict).sum()/float(final_y.shape[0])
-test = pip_rf.transform(test)
-y_test = rfc_self.predict(test)
+	# # ###output the data from y_test
+	output_rf = pd.Series(y_test, index = test.index).reset_index()
+	output_rf.columns = ['PassengerId','Survived']
+	# output_rf.to_csv('y_test.csv', index=False)
+	## roughly checking performance
+	sk = pd.read_csv('y_test_0.csv')
+	print (sk.Survived == y_test).sum()/float(sk.Survived.shape[0])
 
 
-# # ###output the data from y_test
-output = pd.concat([test_passengerId, pd.Series(y_test)], axis=1)
-output.columns = ['PassengerId','Survived']
-output.to_csv('y_test.csv', index=False)
+#########################SVM model#########################
+# cross_validation_svm = True
+cross_validation_svm = False
+
+categorical_vars = ['Pclass','Name','Cabin','Embarked',   'Ticket_start', 'Ticket']
+# categorical_vars = ['Pclass','Name','Cabin','Embarked',     'Ticket']
+
+#####pipeline transformation for SVM
+# in SVM, you should scale, because the RBF kernel of SVM or the L1 and L2 regularizers of linear models
+# assume that all features are centered around 0 and have variance in the same order
+
+# pip_svm=make_pipeline(ProcessFare(), NominalSibSp(), NominalParch(), TransformSex(), \
+# 	ExtractName(), ProcessTicket(ticket_start=False), ProcessTicket2(drop=False),\
+# 	ProcessEmbarked(), ProcessCabin(), CombineDummyVars(categorical_vars), LinearRegForAge(), Scaler())
+
+pip_svm=make_pipeline(ProcessFare(), NominalSibSp(), NominalParch(), TransformSex(), \
+	ExtractName(), ProcessTicket(ticket_start=True), ProcessTicket2(drop=False),\
+	ProcessEmbarked(), ProcessCabin(), CombineDummyVars(categorical_vars), LinearRegForAge())
 
 
+if cross_validation_svm:
+	print 'first print, data df shape is (%d, %d)' %(train.shape[0], train.shape[1])
+	train = pip_svm.fit_transform(train)
+	print 'second print, data df shape is (%d, %d)' %(train.shape[0], train.shape[1])
+	valid = pip_svm.transform(valid)
 
-##################SVM model
+
+	######best parameter setting so far
+# the best parameter setting is: SVC(C=0.1, cache_size=200, class_weight=None, coef0=0.0, degree=3, gamma=0.1,
+#   kernel='linear', max_iter=-1, probability=False, random_state=None,
+#   shrinking=True, tol=0.001, verbose=False)
+# the best CV score of the GridSearchCV is: 0.830532212885
+# the in-bag prediction accuracy rate is: 0.841736694678
+# the validation prediction accuracy rate is: 0.83050847457
+	######best parameter setting so far
+
+	# # ###SVM classifier
+	# SVC(C=1.0, kernel='rbf', degree=3, gamma='auto', coef0=0.0, \
+	# 	shrinking=True, probability=False, tol=0.001, cache_size=200, class_weight=None, \
+	# 	verbose=False, max_iter=-1, decision_function_shape=None, random_state=None)
+	svm = SVC()
+	# #parameter grids:
+	#linear SVM 
+	SVM_C = list(np.arange(0.03, 0.16, 0.01))
+	SVM_gamma = [0.3]
+	# SVM_kernel = ['sigmoid', 'rbf']
+	SVM_kernel = ['linear']
+	#kernel SVM
+	# SVM_C = [0.001, 0.003, 0.1, 0.3, 0.6, 1.0, 3.0, 10, 20]
+	# SVM_gamma = [0.001, 0.003, 0.1, 0.3, 0.6, 1.0, 3.0, 10, 20]
+	# SVM_kernel = ['sigmoid']
+
+
+	SVM_paras = {'C': SVM_C, 'gamma':SVM_gamma,'kernel':SVM_kernel}
+	SVM_grid_search = GridSearchCV(svm, param_grid = SVM_paras, cv = 7, refit=True)
+	SVM_grid_search.fit(train, y_train)
+	# SVM_grid_search.fit(train.astype(np.float), y_train.astype(np.float))
+
+	y_predict = SVM_grid_search.predict(train)
+	y_valid_predict = SVM_grid_search.predict(valid)
+
+	# ###checking results
+	print 'the combinations scores of each parameter setting:'
+	for item in sorted(SVM_grid_search.grid_scores_, key=lambda x: x[1]): print item
+	print 'the best parameter setting is:', SVM_grid_search.best_estimator_
+	print 'the best CV score of the GridSearchCV is:', SVM_grid_search.best_score_
+	#best CV score is:
+	
+	print 'the in-bag prediction accuracy rate is:', (y_train == y_predict).sum()/float(y_train.shape[0])
+	print 'the validation prediction accuracy rate is:', (y_valid == y_valid_predict).sum()/float(y_valid.shape[0])
+	# print 'the feature importances are:', sorted(list(zip(train.columns, rfc_grid_search.best_estimator_.feature_importances_)), key = lambda x: x[1], reverse=True)
+
+
+else:
+	# #####combine train and valid to get the final model and predict again
+	svm_self = SVC(C=0.09, kernel ='linear', gamma= 0.3)
+	# svm_self = SVC(C=20 , kernel ='sigmoid', gamma= 0.003)
+	# final_df = pd.concat([train, valid], axis = 0)
+	final_df = pip_svm.fit_transform(df)
+	final_y = pd.concat([y_train, y_valid], axis=0).reset_index().set_index('PassengerId').sort().loc[:,'Survived']
+	svm_self.fit(final_df,final_y)
+	y_predict = svm_self.predict(final_df)
+	print 'the in-bag prediction accuracy rate of SVM is:', (final_y == y_predict).sum()/float(final_y.shape[0])
+	test_svm = pip_svm.transform(test)
+	y_test = svm_self.predict(test_svm)
+
+
+	# # ###output the data from y_test
+	output_svm = pd.Series(y_test, index = test.index).reset_index()
+	output_svm.columns = ['PassengerId','Survived']
+	output_svm.to_csv('y_test.csv', index=False)
+	## roughly checking performance
+	sk = pd.read_csv('y_test_0.csv')
+	print (sk.Survived == y_test).sum()/float(sk.Survived.shape[0])
 
 
 
